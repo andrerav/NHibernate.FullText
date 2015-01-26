0e45bc0aed5c2366bc371e91f89a70d9f648b3f5
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2015-01-26 20:30:27 +0100

    Upgraded to latest NHibernate version, improved description,

--- src/NHibernate.FullText/Properties/AssemblyInfo.cs
+++ src/NHibernate.FullText/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NHibernate.FullText")]
-[assembly: AssemblyDescription("Provides fulltext query capabilities to NHibernate.")]
+[assembly: AssemblyDescription("Provides fulltext query capabilities to NHibernate. Includes LINQ provider. See https://github.com/andrerav/NHibernate.FullText for how-to.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andreas Ravnestad")]
 [assembly: AssemblyProduct("NHibernate.FullText")]
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3c69e557-b023-44ff-9927-192f59c361d5")]
-[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyVersion("0.1.1.*")]
