081c9b8e463bca301a54e94a6b174cb8a9512d65
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2022-07-06 00:14:46 +0200

    Updated framework monikers, updated NuGet packages, added new pack/publish scripts and removed the old one, bumped version number, removed NuGet executable.

--- src/NHibernate.FullText/BuilPackages.bat
+++
@@ -1,3 +0,0 @@
-if not exist "./NuGet Packages" mkdir "./NuGet Packages"
-del /Q "NuGet Packages\*"
-call "./../.nuget/NuGet.exe" pack NHibernate.FullText.csproj -Build -IncludeReferencedProjects -Properties Configuration=Release -OutputDirectory "./NuGet Packages"
